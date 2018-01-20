9d7d0fe9f28283fd4083a2c519fa4c66684cb621
= <babic.milan999@yahoo.com>
2018-01-20 15:53:43 +0100

    changed homepage and navbar

--- src/app/components/insurance/insurance-data.service.ts
+++ src/app/components/insurance/insurance-data.service.ts
@@ -6,43 +6,43 @@ import {OsiguranjeDTO} from "../../beans/osiguranjeDTO";
 export class InsuranceDataService {
 
 
-  private url = 'http://localhost:8080/';
+  private url = 'http://localhost:8081/corpMain/';
 
   constructor(private http:Http) { }
 
   public getStarosneGrupe(){
-    return this.http.get(this.url+'dmzMain/dobaviStarosneGrupe');
+    return this.http.get(this.url+'dobaviStarosneGrupe');
   }
 
   public getRegioni() {
-    return this.http.get(this.url + 'dmzMain/dobaviRegione');
+    return this.http.get(this.url + 'dobaviRegione');
   }
 
   public getSvrheOsiguranja() {
-    return this.http.get(this.url + 'dmzMain/dobaviSvrheOsiguranja');
+    return this.http.get(this.url + 'dobaviSvrheOsiguranja');
   }
 
   public getPaketiOsiguranja() {
-    return this.http.get(this.url + 'dmzMain/dobaviPaketeOsiguranja');
+    return this.http.get(this.url + 'dobaviPaketeOsiguranja');
   }
 
   public getStarostiStana() {
-    return this.http.get(this.url + 'dmzMain/dobaviStarostiStana');
+    return this.http.get(this.url + 'dobaviStarostiStana');
   }
 
   public getProcenjeneVrednostiStana() {
-    return this.http.get(this.url + 'dmzMain/dobaviProcenjeneVrednostiStana');
+    return this.http.get(this.url + 'dobaviProcenjeneVrednostiStana');
   }
 
   public getOsiguranjaStana() {
-    return this.http.get(this.url + 'dmzMain/dobaviOsiguranjaStana');
+    return this.http.get(this.url + 'dobaviOsiguranjaStana');
   }
 
   public saveInsurance(osiguranjeDTO : OsiguranjeDTO) {
     const headers = new Headers();
     headers.append('Content-Type' , 'application/json');
     console.log(osiguranjeDTO.nekretnine);
-    return this.http.post(this.url + 'dmzMain/saveInsurance', JSON.stringify(osiguranjeDTO), {headers : headers});
+    return this.http.post(this.url + 'saveInsurance', JSON.stringify(osiguranjeDTO), {headers : headers});
   }
 
 }
